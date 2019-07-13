c1d68001e3f32fb7e4e00577bc9c074a191d6172
momokASC6 <mikuniku3@gmail.com>
2019-07-13 19:20:28 -0400

    Adds comments

--- mash.js
+++ mash.js
@@ -75,37 +75,41 @@
 // }
 
 /* VERSION 5 */
+
+// Defining the home and car arrays for our MASH game
 let homes = ["Mansion", "Apartment", "Shack", "House", "New Jersey"];
 
 let cars = ["lambo", "convertible", "flintstones car", "cloud", "box with wheels"];
 
 function mash() {
+    // Our mash function returns a string that has random values from our other functions
     return "You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids!" + ", and you’ll drive a " + getCar();
 }
 
 function randomizer(range) {
+    // Our randomizer function returns a random number between a range of 0 and range parameter
     return Math.floor(Math.random() * (range + 1));
 }
 
 function getHome() {
-    if(process.argv[2] != "")
-        homes.push(process.argv[2]);
-    return homes[randomizer(homes.length-1)];
+    if(process.argv[2] != "") // Check if user input exists first
+        homes.push(process.argv[2]); // If so then push to the possible outcomes for a home
+    return homes[randomizer(homes.length-1)]; // Return a random home
 }
 
 function getChildrenCount() {
-    if(process.argv[3] != "" || Math.random() >= 0.5)
+    if(process.argv[3] != "" || Math.random() >= 0.5) // If no input exists for children, then we will return a random number
         return randomizer(100);
-    else
+    else // Otherwise there is a chance for the player to get their specified number of children
         return process.argv[3];
 
 }
 
 function getCar() {
-    if(process.argv[4]!= "" || Math.random() >= 0.5)
+    if(process.argv[4]!= "" || Math.random() >= 0.5) // If not input exists for a car, then we will return a random car
         return cars[randomizer(cars.length-1)];
-    else
+    else // Otherwise the user has a chance for the player to get the car they entered
         return process.argv[4];
 }
 
-console.log(mash());
+console.log(mash()); // Play our game. Notice how when we call mash() inside console.log it runs mash and then the funcionts we call in mash
